1b4c2d05780e34eaed9fafc4404ede2f06669692
20urc3 <94982366+20urc3@users.noreply.github.com>
2024-03-18 16:34:06 +0100

    Create retrieve_ipa.py

---
+++ iOS/retrieve_ipa.py
@@ -0,0 +1,61 @@
+import paramiko, sys, os, re
+
+def ssh_command(ip, password, command):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, username='root', password=password)
+    stdin, stdout, stderr = client.exec_command(command)
+    output = stdout.read().decode()
+    error = stderr.read().decode()
+    if error:
+        print("Error:", error)
+    client.close()
+    return output
+
+def sftp_get(ip, password, remote_path, local_path):
+    transport = paramiko.Transport((ip, 22))
+    transport.connect(username='root', password=password)
+    sftp = paramiko.SFTPClient.from_transport(transport)
+    sftp.get(remote_path, local_path)
+    transport.close()
+
+def main():
+    parser = argparse.ArgumentParser(description="Extract an iOS app from an iPhone over SSH")
+    parser.add_argument("IP", help="IP address of the iPhone")
+    parser.add_argument("password", help="SSH password")
+    parser.add_argument("app_name", help="Name of the iOS app")
+    parser.add_argument("name_to_extract", help="Name to use for the extracted IPA file")
+    args = parser.parse_args()
+
+    ip = args.IP
+    password = args.password
+    app_name = args.app_name
+    name_to_extract = args.name_to_extract
+
+    # SSH commands
+    app_path_command = f"cd /var/containers/Bundle/Application/ && find . -type d -name '*{app_name}*' | head -n 1"
+    app_path_output = ssh_command(ip, password, app_path_command)
+    match = re.search(r'(\./[0-9A-F\-]+)', app_path_output)
+    if not match:
+        print("Error: Couldn't find app path")
+        sys.exit(1)
+
+    app_uuid_path = match.group(1)
+    payload_path = f"{app_uuid_path}/Payload"
+    app_bundle_path = f"{app_uuid_path}/{app_name}.app"
+    ipa_name = f"/var/root/{name_to_extract}.ipa"
+
+    mkdir_payload_command = f"mkdir -p {payload_path}"
+    ssh_command(ip, password, mkdir_payload_command)
+
+    copy_app_command = f"cp -r {app_bundle_path} {payload_path}/"
+    ssh_command(ip, password, copy_app_command)
+
+    zip_command = f"zip -r {ipa_name} {payload_path}"
+    ssh_command(ip, password, zip_command)
+
+    # SFTP commands
+    sftp_get(ip, password, ipa_name, f"{name_to_extract}.ipa")
+
+if __name__ == "__main__":
+    main()
